829d70b13fbdf9dc43baef81b77f2454c0c95067
Rafeyfa Naqeeb <75088453+rafeyfa@users.noreply.github.com>
2021-11-26 13:41:58 +0700

    Delete constants.ts

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-import { homedir } from "os";
-export const HOME_PATH = homedir();
-export const PROJECT_ROOT_PATH = __dirname + '/../';
-export const VERSION = "v1.0.1";
-export const ONE_LINE_CHANGELOG = "UserAgent Generator.";
-export const IS_DEV = process.env.NODE_ENV && process.env.NODE_ENV === "development";
