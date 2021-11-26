d7a10e643bc7533fef43866a2b12b517470048a7
Rafeyfa Naqeeb <75088453+rafeyfa@users.noreply.github.com>
2021-11-26 13:20:24 +0700

    Delete lib directory

--- lib/README.md
+++
@@ -1,2 +0,0 @@
-# generate-useragent-node
-Simple useragents generator.
--- lib/constants.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const HOME_PATH: string;
-export declare const PROJECT_ROOT_PATH: string;
-export declare const VERSION = "v1.0.1";
-export declare const ONE_LINE_CHANGELOG = "UserAgent Generator.";
-export declare const IS_DEV: boolean;
