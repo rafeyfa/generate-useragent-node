0040e77622905573ef1c3517d3ca6137809aa70a
Rafeyfa Naqeeb <75088453+rafeyfa@users.noreply.github.com>
2021-12-02 05:43:46 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as devices from './useragent.json';
 export function userAgentFactory(seed: string){
 	const count   = Object.keys(devices).length;
 	const chance  = new Chance(seed);
-	const pick    = chance.integer({ max: count });
+	const pick    = chance.integer({ min: 0, max: count });
 
 	return devices[pick];
 }
